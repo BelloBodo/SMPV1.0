fba45cd996898ec8c8c44bc35a7d4a1cd92a7f3c
BelloBodo <bellobodo@gmail.com>
2023-08-07 18:06:12 +0200

    Initial commit

---
+++ src/main/java/de/bellobodo/smpv1/SMPV1.java
@@ -0,0 +1,17 @@
+package de.bellobodo.smpv1;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SMPV1 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
